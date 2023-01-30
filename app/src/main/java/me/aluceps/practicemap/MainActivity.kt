84ceaae6e4f389014501b927716ee8b2ca7a5339
aluceps <aluceps@gmail.com>
2023-01-30 17:15:45 +0900

    setup map position

--- app/src/main/java/me/aluceps/practicemap/MainActivity.kt
+++ app/src/main/java/me/aluceps/practicemap/MainActivity.kt
@@ -2,8 +2,12 @@ package me.aluceps.practicemap
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import me.aluceps.practicemap.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -22,6 +26,10 @@ class MainActivity : AppCompatActivity() {
 
         val mapFragment = SupportMapFragment.newInstance()
         val callback = OnMapReadyCallback {
+            val tokyo = LatLng(35.6892864, 139.6899497)
+            val zoom = 10f
+            it.addMarker(MarkerOptions().position(tokyo).title("tokyo"))
+            it.moveCamera(CameraUpdateFactory.newCameraPosition(CameraPosition.fromLatLngZoom(tokyo, zoom)))
         }
 
         mapFragment.getMapAsync(callback)
