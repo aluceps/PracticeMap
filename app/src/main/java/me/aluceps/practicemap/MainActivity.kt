a813060b7cec16125dcc8fa5d0b56660e817b991
aluceps <aluceps@gmail.com>
2023-01-30 14:45:39 +0900

    add callback

--- app/src/main/java/me/aluceps/practicemap/MainActivity.kt
+++ app/src/main/java/me/aluceps/practicemap/MainActivity.kt
@@ -2,6 +2,7 @@ package me.aluceps.practicemap
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import me.aluceps.practicemap.databinding.ActivityMainBinding
 
@@ -11,11 +12,16 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    val callback = OnMapReadyCallback {
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         val mapFragment = SupportMapFragment.newInstance()
+        mapFragment.getMapAsync(callback)
+
         supportFragmentManager.beginTransaction()
             .add(R.id.container, mapFragment)
             .commit()
